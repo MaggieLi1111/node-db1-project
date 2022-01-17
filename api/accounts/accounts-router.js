34ae7abd2ff636c61fe1f46acc93711f7032218c
unknown <lidanyang1111@gmail.com>
2022-01-17 11:50:20 -0800

    version2

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,58 @@
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
+const md = require("./accounts-middleware")
+
+const Account = require("./accounts-model")
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
+
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json("post account")
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+
+router.put('/:id', md.checkAccountNameUnique, md.checkAccountId, md.checkAccountPayload, (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json("update account")
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json("delete account by id")
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message:err.message
+  })
 })
 
 module.exports = router;
